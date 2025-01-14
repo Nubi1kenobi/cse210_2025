42deae3eb70212744a29b9eb285edda130113269
Bryan C. Stauch <sta15060@byui.edu>
2025-01-13 22:44:23 -0600

    Hello Prep3 World! - Complete!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = 0;
+        int lowerLimit = 1;
+        int upperLimit = 12;
+        int magicKey = randomNumber.Next(lowerLimit,upperLimit + 1);
+
+        Console.Clear();
+        while (magicNumber != magicKey)
+        {
+            Console.Write("What is the magic number? ");
+            string magicString = Console.ReadLine();
+            magicNumber = int.Parse(magicString);  //unser string input parsed to an integer
+            if (magicNumber > upperLimit) 
+            {
+                Console.WriteLine("Out of bounds!");
+                Console.WriteLine("Prest anykey to continue.");
+                Console.ReadKey();
+                Console.Clear();    
+            }
+            else if (magicNumber < lowerLimit) 
+            {
+                Console.WriteLine("Out of bounds!");
+                Console.WriteLine("Prest anykey to continue.");
+                Console.ReadKey();
+                Console.Clear();    
+            }
+            else if (magicNumber < magicKey) 
+            {
+                Console.WriteLine("HIGHER!");
+                Console.WriteLine("Prest anykey to continue.");
+                Console.ReadKey();
+                Console.Clear();    
+            }
+            
+            else if (magicNumber > magicKey) 
+            {
+                Console.WriteLine("LOWER!");
+                Console.WriteLine("Prest anykey to continue.");
+                Console.ReadKey();
+                Console.Clear();    
+            }            
+        
+        }
+        Console.Write("Winner Winner Chicken Dinner!");
+
+
     }
 }
